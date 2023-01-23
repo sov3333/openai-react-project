445856042551d4ef658e4f593ccd183287ec7da2
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-01-23 12:37:10 +0800

    add normalize css, setup app sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
+import './normal.css'; // https://necolas.github.io/normalize.css/8.0.1/normalize.css
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+      <section className="chatbox">
+
+      </section>
     </div>
   );
 }
