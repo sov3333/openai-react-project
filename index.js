d8533a8aa9baf65010927cefbb37a1d8a65e12ba
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-01-23 14:32:08 +0800

    create frontend api request using fetch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const configuration = new Configuration({
     organization: "org-yMNcNoBzCrexrK5tw3IlzN3B",
@@ -9,18 +11,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 3080;
 
 app.post('/', async (req, res) => {
+    const { message } = req.body;
+    console.log(message);
+
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
     });
-    console.log(response.data.choices[0].text)
     res.json({
-        data: response.data
+        message: response.data.choices[0].text
     })
 });
 
